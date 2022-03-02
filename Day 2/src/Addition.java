d670f9fb072a9ad8241ba3fbd827527bd027e7e8
Home-PC <Home-PC@LAPTOP-VIT8I0BK>
2022-03-02 18:19:46 +0530

    first commit

---
+++ Day 2/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20909;
+		int b=20;
+		int c=a+b;
+		System.out.print("Sum of a+b is : "+c);
+	}
+
+}
